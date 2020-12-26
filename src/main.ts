90509bd3667e94fe276901a7025f02c3d2228d97
Victor Anderssén <victor.anderssen@gmail.com>
2020-12-26 18:51:16 +0000

    Cleanup, error fixes

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
-import fs from 'fs'
 import { Client, VoiceChannel } from 'discord.js'
-import { createCommand, ICommand, ICommandConstructor } from './interfaces/command'
 import { Queue } from './queue'
 import { AudioDispatcher } from './audioDispatcher'
-import { checkTimezoneSettings } from './utils'
-import { createEvent, IEvent, IEventConstructor } from './interfaces/event'
+import { checkTimezoneSettings, loadCommandFiles, loadEvents } from './utils'
+
 const Discord = require('discord.js')
 export const client: Client = new Discord.Client()
 export const clientCommands = new Discord.Collection()
+
 
 // Global queue for storing audio sources
 export const audioQueue: Queue<Map<string, VoiceChannel>> = new Queue()
@@ -20,46 +19,9 @@ audioDispatcher.initialize()
 checkTimezoneSettings()
 
 // Bind all commands
-async function loadCommandFiles(): Promise<any> {
-    const commandFiles = fs.readdirSync('dist/commands').filter(file => file.endsWith('.js'))
-    for (const file of commandFiles) {
-        try {
-            const command: ICommand = await require(`./commands/${file}`)
-            const newCommand: ICommand = createCommand(command as unknown as ICommandConstructor, command.name, command.description)
-            clientCommands.set(newCommand.name, newCommand)
-        } catch (error) {
-            console.error(`loadCommandFiles: ${error}`)
-        }
-    }
-}
-
 loadCommandFiles()
 
-
 // Bind all events
-async function loadEvents(): Promise<any> {
-    console.log(`------------------- Loading events -----------------------`)
-    const eventFiles = fs.readdirSync('dist/events').filter(file => file.endsWith('.js'))
-    for (const file of eventFiles) {
-        try {
-            const eventName: string = file.split('.')[0]; // Get the exact name of the event from the eventFunction variable. If it's not given, the code just uses the name of the file as name of the event
-            const event: IEvent = await require(`./events/${file}`)
-            const newEvent: IEvent = createEvent(event as unknown as IEventConstructor)
-            console.log(`Event: ${file}`)
-            const emitter = client; // Here we define our emitter. This is in our case the client (the bot)
-            try {
-                emitter['on'](eventName, (...args: any) => newEvent.execute(...args)) // Run the event using the above defined emitter (client)
-            } catch (error) {
-                console.error(error.stack); // If there is an error, console log the error stack message
-            }
-        } catch (error) {
-            console.error(`loadEvents: ${error}`)
-        }
-    }
-    console.log(`----------------------------------------------------------`)
-}
-
 loadEvents()
-
 
 client.login(process.env.token)
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,8 @@
+import fs from 'fs'
 import { utcToZonedTime } from "date-fns-tz"
+import { createCommand, ICommand, ICommandConstructor } from "./interfaces/command"
+import { client, clientCommands } from "./main"
+import { createEvent, IEvent, IEventConstructor } from './interfaces/event'
 
 
 export function checkTimezoneSettings(): void {
@@ -24,4 +28,41 @@ export function checkTimezoneSettings(): void {
             process.exit(1)
         }
     }
+}
+
+
+export async function loadCommandFiles(): Promise<any> {
+    const commandFiles = fs.readdirSync('dist/commands').filter(file => file.endsWith('.js'))
+    for (const file of commandFiles) {
+        try {
+            const command: ICommand = await require(`./commands/${file}`)
+            const newCommand: ICommand = createCommand(command as unknown as ICommandConstructor, command.name, command.description)
+            clientCommands.set(newCommand.name, newCommand)
+        } catch (error) {
+            console.error(`loadCommandFiles: ${error}`)
+        }
+    }
+}
+
+
+export async function loadEvents(): Promise<any> {
+    console.log(`------------------- Loading events -----------------------`)
+    const eventFiles = fs.readdirSync('dist/events').filter(file => file.endsWith('.js'))
+    for (const file of eventFiles) {
+        try {
+            const eventName: string = file.split('.')[0]; // Get the exact name of the event from the eventFunction variable. If it's not given, the code just uses the name of the file as name of the event
+            const event: IEvent = await require(`./events/${file}`)
+            const newEvent: IEvent = createEvent(event as unknown as IEventConstructor)
+            console.log(`Event: ${file}`)
+            const emitter = client; // Here we define our emitter. This is in our case the client (the bot)
+            try {
+                emitter['on'](eventName, (...args: any) => newEvent.execute(...args)) // Run the event using the above defined emitter (client)
+            } catch (error) {
+                console.error(error.stack); // If there is an error, console log the error stack message
+            }
+        } catch (error) {
+            console.error(`loadEvents: ${error}`)
+        }
+    }
+    console.log(`----------------------------------------------------------`)
 }
