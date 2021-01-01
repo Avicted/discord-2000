522134389c286ac7c3a4edc6780a4ae69623a5ae
Victor Anderssén <victor.anderssen@gmail.com>
2021-01-01 06:32:57 +0200

    Bugfix: migration case sensitivity,

--- src/persistence/migration/1609343793765-InitialUserModel.ts
+++ src/persistence/migration/1609343793765-InitialUserModel.ts
@@ -1,14 +1,15 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm'
 
 export class InitialUserModel1609343793765 implements MigrationInterface {
     name = 'InitialUserModel1609343793765'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "user" ("id" character varying NOT NULL, "displayName" character varying NOT NULL, "nickName" character varying NOT NULL, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
+        await queryRunner.query(
+            `CREATE TABLE "user" ("id" character varying NOT NULL, "displayName" character varying NOT NULL, "nickname" character varying NOT NULL, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "user"`);
+        await queryRunner.query(`DROP TABLE "user"`)
     }
-
 }
--- src/persistence/migration/1609417667816-UserNicknameIsNowNullable.ts
+++ src/persistence/migration/1609417667816-UserNicknameIsNowNullable.ts
@@ -1,22 +1,21 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm'
 
 export class UserNicknameIsNowNullable1609417667816 implements MigrationInterface {
     name = 'UserNicknameIsNowNullable1609417667816'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`COMMENT ON COLUMN "user_presence"."createdAt" IS NULL`);
-        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "nickName" DROP NOT NULL`);
-        await queryRunner.query(`COMMENT ON COLUMN "user"."nickName" IS NULL`);
-        await queryRunner.query(`COMMENT ON COLUMN "user"."createdAt" IS NULL`);
-        await queryRunner.query(`COMMENT ON COLUMN "audio_command"."createdAt" IS NULL`);
+        await queryRunner.query(`COMMENT ON COLUMN "user_presence"."createdAt" IS NULL`)
+        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "nickname" DROP NOT NULL`)
+        await queryRunner.query(`COMMENT ON COLUMN "user"."nickname" IS NULL`)
+        await queryRunner.query(`COMMENT ON COLUMN "user"."createdAt" IS NULL`)
+        await queryRunner.query(`COMMENT ON COLUMN "audio_command"."createdAt" IS NULL`)
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`COMMENT ON COLUMN "audio_command"."createdAt" IS NULL`);
-        await queryRunner.query(`COMMENT ON COLUMN "user"."createdAt" IS NULL`);
-        await queryRunner.query(`COMMENT ON COLUMN "user"."nickName" IS NULL`);
-        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "nickName" SET NOT NULL`);
-        await queryRunner.query(`COMMENT ON COLUMN "user_presence"."createdAt" IS NULL`);
+        await queryRunner.query(`COMMENT ON COLUMN "audio_command"."createdAt" IS NULL`)
+        await queryRunner.query(`COMMENT ON COLUMN "user"."createdAt" IS NULL`)
+        await queryRunner.query(`COMMENT ON COLUMN "user"."nickname" IS NULL`)
+        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "nickname" SET NOT NULL`)
+        await queryRunner.query(`COMMENT ON COLUMN "user_presence"."createdAt" IS NULL`)
     }
-
 }
